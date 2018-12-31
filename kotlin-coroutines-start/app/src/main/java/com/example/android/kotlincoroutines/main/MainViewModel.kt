bc4e0f39b3a99807375ee76645706d507522e39e
Yehan Pemarathne <yehancha@gmail.com>
2018-12-31 11:43:05 +0530

    Do Section 5

--- kotlin-coroutines-start/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ kotlin-coroutines-start/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -20,6 +20,7 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.example.android.kotlincoroutines.util.BACKGROUND
+import kotlinx.coroutines.*
 
 /**
  * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
@@ -45,20 +46,33 @@ class MainViewModel : ViewModel() {
     val snackbar: LiveData<String>
         get() = _snackBar
 
-    // TODO: Add viewModelJob and uiScope here
+    private val viewModelJob = Job()
 
-    // TODO: Add onCleared() here to cancel viewModelJob
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 
     /**
      * Wait one second then display a snackbar.
      */
     fun onMainViewClicked() {
-        // TODO: Replace with coroutine implementation
-        BACKGROUND.submit {
-            Thread.sleep(1_000)
-            // use postValue since we're in a background thread
-            _snackBar.postValue("Hello, from threads!")
+        // launch a coroutine in uiScope
+        uiScope.launch {
+            // suspend this coroutine for one second
+            delay(1_000)
+            // resume in the main dispatcher
+            // _snackbar.value can be called directly from main thread
+            _snackBar.value = "Hello, from coroutines!"
         }
+
+//        BACKGROUND.submit {
+//            Thread.sleep(1_000)
+//            // use postValue since we're in a background thread
+//            _snackBar.postValue("Hello, from threads!")
+//        }
     }
 
     /**
